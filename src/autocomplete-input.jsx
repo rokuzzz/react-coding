62d999b4d4ebb250308d48c168b426a5887fc5f8
Roman Kuzero <rombeldan@gmail.com>
2024-11-26 04:58:31 +0200

    implement basic autocomplete functionality

--- src/autocomplete-input.jsx
+++ src/autocomplete-input.jsx
@@ -1,17 +1,45 @@
-import React from "react";
-import AutocompleteTextbox from "./helper-components/autocomplete-textbox";
-import AutocompleteSuggestion from "./helper-components/autocomplete-suggestion";
-import apiClient from "./helper-components/api-client";
+import React, { useState } from 'react';
+import AutocompleteTextbox from './helper-components/autocomplete-textbox';
+import AutocompleteSuggestion from './helper-components/autocomplete-suggestion';
+import apiClient from './helper-components/api-client';
 
 const AutocompleteInput = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+
+  const handleInputChange = async (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+
+    if (value.trim() !== '') {
+      try {
+        const results = await apiClient.search(value);
+        setSuggestions(results);
+      } catch (error) {
+        console.error('Error fetching suggestions:', error);
+        setSuggestions([]);
+      }
+    } else {
+      setSuggestions([]);
+    }
+  };
+
   return (
     <div>
       <AutocompleteTextbox
-        onChange={(event) => {
-          // TODO: fetch suggestions with apiClient.search
-        }}
+        onChange={handleInputChange}
+        value={inputValue}
+        aria-label='Search for an option'
       />
-      {/* TODO: render suggestions here with AutocompleteSuggestion */}
+      {suggestions.length > 0 && (
+        <div role='listbox' className='suggestions-list'>
+          {suggestions.map((suggestion, index) => (
+            <AutocompleteSuggestion key={index} role='option'>
+              {suggestion}
+            </AutocompleteSuggestion>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
